12d41f1ce5baa18889f343bbfaff4567ab5b6da2
Luke Fretwell <luke@lukefretwell.com>
2026-06-29 07:47:09 -0700

    Fetch audits.json from GitHub, remove local flag

--- _data/audits.js
+++ _data/audits.js
@@ -1,10 +1,9 @@
-import * as fs from 'fs';
 import { getData } from '../scripts/getdata.js';
 
 export default async function () {
   let auditsFile = 'https://github.com/ScanGov/data/raw/refs/heads/main/standards/audits.json';
 
-  const auditData = await getData(auditsFile, true);
+  const auditData = await getData(auditsFile);
   return auditData;
 }
 
